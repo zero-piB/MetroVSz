527384e1387f6ac1f74bad377792bd8a3b317e72
zero <13260648390@163.com>
2020-03-20 00:38:57 +0800

    安装依赖环境

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <header class="header">
+			<h3>公交网络服务水平系统</h3>
+		</header>
+    <div class="wapper">
+      <button class="btn btn-primary">主要的</button>
+<button class="btn btn-success">成功的</button>
+<button class="btn btn-danger">危险的</button>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
   }
 }
+</script>
+<style lang="scss">
+
 </style>
