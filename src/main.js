2ac55ab734daf79a8e656417c412186880c0e3d7
zero <13260648390@163.com>
2020-04-05 15:08:32 +0800

    移除vue-router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import './plugins/bootstrap-vue'
 import './plugins/element-ui'
@@ -11,7 +10,6 @@ import './plugins/axios'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
